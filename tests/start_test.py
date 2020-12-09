db1a4a64eaac6ae05be5c9c0c7d98c365c2afadb
Jin <qinjin@bu.edu>
2020-12-08 22:44:12 -0800

    fix: typo

--- tests/start_test.py
+++ tests/start_test.py
@@ -155,7 +155,7 @@ if __name__ == '__main__':
         print('valid requests: {}'.format(valid_http_req))
         print('total requests: {}'.format(total_http_req))
         print('goodput: {} sec'.format(goodput))
-        print('throughput: {} sec'.fomrat(throughput))
+        print('throughput: {} sec'.format(throughput))
 
         print("finish this turn")
         print('----------------------------------------------------------')
