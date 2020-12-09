4594549af929cdf5c66feedb7bd6004404e07585
qinjin <qinjin@worker-3.geni-mini-project.ch-geni-net.instageni.cenic.net>
2020-12-08 18:51:22 -0800

    fix: worker error while image channel is one update: worker output -1 if error occurs.

--- worker/run_mobilenet_v2.py
+++ worker/run_mobilenet_v2.py
@@ -12,21 +12,24 @@ if __name__ == '__main__':
     from PIL import Image
     from torchvision import transforms
 
-    input_image = Image.open(sys.argv[1])
-    preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-    input_tensor = preprocess(input_image)
-    input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+    try :
+        input_image = Image.open(sys.argv[1]).convert(mode='RGB')
+        preprocess = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        input_tensor = preprocess(input_image)
+        input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
 
-    with torch.no_grad():
-        output = model(input_batch)
-    
-    # get probabilities
-    probs = torch.nn.functional.softmax(output[0], dim=0)
+        with torch.no_grad():
+            output = model(input_batch)
+        
+        # get probabilities
+        probs = torch.nn.functional.softmax(output[0], dim=0)
 
-    # get prediction
-    print(torch.argmax(probs).item())
+        # get prediction
+        print(torch.argmax(probs).item())
+    except:
+        print('-1')
